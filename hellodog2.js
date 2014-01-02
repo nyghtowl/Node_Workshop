ca53d5bf363f0a16b56119a0b9c71f19edcacdbb
nyghtowl <warrick.melanie@gmail.com>
2014-01-02 11:18:36 -0800

    Added hello dog with two callbacks.

---
+++ hellodog2.js
@@ -0,0 +1,14 @@
+// 2nd Example - Shows two callbacks
+ 
+var http = require('http');
+
+http.createServer(function(request, response) {
+	response.writeHead(200);
+	response.write("Dog is running.");
+	setTimeout(function(){
+		response.write("Dog is done.")		
+	response.end();
+	}, 5000);
+}).listen(8080);
+
+console.log("Listening on port 8080...")
